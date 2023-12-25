1e51bbedef3784a5cc75d05f6c4ab1206ef57823
RaptoRRmakesGames <gjkafantaris@gmail.com>
2023-12-25 19:24:06 +0200

    finished site skeleton

--- app.py
+++ app.py
@@ -4,6 +4,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page="home")
+
+@app.route("/blogs")
+def blogs():
+    return render_template("blog.html", page="blog")
+
+@app.route("/write")
+def write():
+    return render_template("write.html", page="write")
+
 
 app.run()
